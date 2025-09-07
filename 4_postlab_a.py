9c72e8d5215eb4d7106b5e4e27194468fdfd58d6
Krish15 <krishmendpara3@gmail.com>
2025-09-07 23:45:28 +0530

    Add files via upload

---
+++ 4_postlab_a.py
@@ -0,0 +1,13 @@
+list = [2, 3, 4, 5, 6, 7, 8, 9]
+result = 1
+for i in list:
+    result *= i
+print(result)
+
+
+
+
+
+
+
+
---
+++ 4_postlab_f.py
@@ -0,0 +1,2 @@
+list = [11, 33, 50]
+print(int(''.join(map(str, list))))
