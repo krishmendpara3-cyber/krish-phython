6e1f32d372bcbc10519b64b58e53d2ac7f48ea71
Krish15 <krishmendpara3@gmail.com>
2025-11-22 22:09:01 +0530

    Add files via upload

---
+++ 27_postlab_a.py
@@ -0,0 +1,10 @@
+
+
+import pandas as pd
+import seaborn as sns
+ 
+# initialise data of lists
+data = {'Name':[ 'Mohe' , 'Karnal' , 'Yrik' , 'jack' ],
+        'Age':[ 30 , 21 , 29 , 28 ]}
+df = pd.DataFrame( data )
+sns.boxplot( data['Age'] )
---
+++ 27_postlab_b.py
@@ -0,0 +1,9 @@
+
+
+import pandas as pd
+import seaborn as sns
+# initialise data of lists
+data = {'Name':[ 'Mohe' , 'Karnal' , 'Yrik' , 'jack' ],
+        'Age':[ 30 , 21 , 29 , 28 ]}
+df = pd.DataFrame( data )
+sns.violinplot(data['Age'])
